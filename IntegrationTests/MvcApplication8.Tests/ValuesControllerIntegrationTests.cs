192fef017049fe8abdd6b3ac99efb735b774c312
litodam <pablo.damiani@southworks.net>
2013-01-29 14:43:41 -0300

    initial commit

---
+++ IntegrationTests/MvcApplication8.Tests/ValuesControllerIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿namespace MvcApplication8.Tests
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MvcApplication8.Controllers;
+    using Newtonsoft.Json.Linq;
+
+    [TestClass]
+    public class ValuesControllerIntegrationTests : WebApiClassBase
+    {
+        public ValuesControllerIntegrationTests() : base("localhost", 8080, typeof(ValuesController)) 
+        {
+        }
+
+        [TestMethod]
+        public void ShouldGetValues()
+        {
+            try
+            {
+                this.Start();
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:8080/");
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));            
+
+                    HttpResponseMessage resp = client.GetAsync("api/values").Result;
+                    resp.EnsureSuccessStatusCode();
+
+                    var jsonResult = resp.Content.ReadAsStringAsync().Result;
+                    
+                    var result = JArray.Parse(jsonResult);
+                    Assert.IsTrue(result.Count == 2);
+                    Assert.AreEqual("value1", result[0]);
+                    Assert.AreEqual("value2", result[1]);
+
+                    Assert.IsFalse(string.IsNullOrEmpty(jsonResult));
+                }
+            }
+            finally
+            {
+                this.Close();   
+            }
+        }
+    }
+}
---
+++ IntegrationTests/MvcApplication8.Tests/WebApiTestableBase.cs
@@ -0,0 +1,129 @@
+﻿namespace MvcApplication8.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Reflection;
+    using System.Web.Http;
+    using System.Web.Http.Dispatcher;
+    using System.Web.Http.SelfHost;
+
+    public abstract class WebApiClassBase : IDisposable
+    {
+        private readonly string baseAddress;
+        private readonly Type controllerType;
+
+        private HttpSelfHostConfiguration configuration;
+        private HttpSelfHostServer server;
+
+        protected WebApiClassBase(Type controllerType) : this("localhost", 8080, controllerType)
+        {
+        }
+
+        protected WebApiClassBase(string host, int port, Type controllerType)
+        {
+            this.controllerType = controllerType;
+            if (string.IsNullOrEmpty(host))
+            {
+                host = "localhost";
+            }
+
+            this.baseAddress = string.Format("http://{0}:{1}", host, port);
+        }
+
+        public virtual HttpSelfHostConfiguration Configuration
+        {
+            get
+            {
+                if (this.configuration == null)
+                {
+                    this.configuration = new HttpSelfHostConfiguration(this.baseAddress);
+                    this.configuration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+                    this.configuration.Services.Replace(typeof(IAssembliesResolver), new TestAssemblyResolver(this.controllerType));
+                    this.configuration.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+                }
+
+                return this.configuration;
+            }
+        }
+
+        public virtual HttpSelfHostServer Server
+        {
+            get { return this.server ?? (this.server = new HttpSelfHostServer(this.Configuration)); }
+        }
+
+        public string BaseAddress
+        {
+            get { return this.baseAddress; }
+        }
+
+        public void Start()
+        {
+            this.Server.OpenAsync().Wait();
+        }
+
+        public void Close()
+        {
+            this.Server.CloseAsync().Wait();
+        }
+
+        public void Dispose()
+        {
+            if (this.configuration != null)
+            {
+                this.configuration.Dispose();
+                this.configuration = null;
+            }
+
+            if (this.server != null)
+            {
+                this.server.Dispose();
+                this.server = null;
+            }
+        }
+
+        protected HttpResponseMessage CreateRequest(string url, HttpMethod method, string acceptedMediaType = null)
+        {
+            var request = new HttpRequestMessage();
+
+            request.RequestUri = new Uri(this.baseAddress + url);
+
+            if (acceptedMediaType != null)
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(acceptedMediaType));
+            }
+
+            request.Method = method;
+
+            var client = new HttpClient(this.Server);
+            using (HttpResponseMessage response = client.SendAsync(request).Result)
+            {
+                return response;
+            }
+        }
+
+        public class TestAssemblyResolver : IAssembliesResolver
+        {
+            private readonly Type controllerType;
+
+            public TestAssemblyResolver(Type controllerType)
+            {
+                this.controllerType = controllerType;
+            }
+
+            public ICollection<Assembly> GetAssemblies()
+            {
+                List<Assembly> baseAssemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+
+                if (!baseAssemblies.Contains(this.controllerType.Assembly))
+                {
+                    baseAssemblies.Add(this.controllerType.Assembly);
+                }
+
+                return baseAssemblies;
+            }
+        }
+    }
+}
